79c327cf2c658b714df0ccaca02e99cef6aa36f0
Nikolai <nikdenof@gmail.com>
2023-09-23 21:04:48 +0300

    The program is updated with multiplication by positive constant, readme is updated with the description of the script

--- ch_1/README.md
+++ ch_1/README.md
@@ -9,3 +9,20 @@ Suppose we take all the weights and biases in a network of perceptrons, and mult
 Show that the behaviour of the network doesn't change.
 
 ### Python script `ex_0_sigmoid` 
+In this script the network of perceptrons is made of 3 inputs, 2 perceptron-neurons on the level 0 and 1 perceptron-neuron on the second level
+which makes the final output. All the weigths are generated randomly and are integers. Each perceptron-neuron gives one binary output (0 or 1).
+
+#### Parameters
+- `LEN_INPUTS` - number of inputs which are generated randomly 
+- `NUM_L0_NEURONS` - number of neurons on the level 0 of perceptron-network 
+- `C` - constant which is used to multiply the weights and the biases. Should be positive.
+
+#### Results
+After testing and extensive logging the script works correctly. Using the data of 20 runs it can be safely assumped that multipling the weights
+by a positive constant `c` gives the same results as original weigths. As I understand it is the case because the output of each perceptron-neuron depends
+on the sign (`+` or `-`) of operation and multipling the $w \cdot c$ and $b \cdot c$ won't change the sign of the the operation before step function. 
+
+Visually:
+- $f(w \cdot x + b)$
+- $f((w \cdot c) \cdot x + (b \cdot c)) = f(c \cdot (w \cdot x + b))$
+- $f(w \cdot x + b) = f(c \cdot (w \cdot x + b))
--- ch_1/ex_0_sigmoid.py
+++ ch_1/ex_0_sigmoid.py
@@ -1,24 +1,26 @@
 import logging
-from math import log
 from random import randint
 
 
-# todo: make len_inputs random
 LEN_INPUTS = 3
 NUM_L0_NEURONS = 2
+C = 3
 
 
 def main():
     log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    logging.basicConfig(level=logging.DEBUG, format=log_fmt)
-    results = build_perceptron_network(
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    results, results_c = build_perceptron_network(
             len_inputs=LEN_INPUTS, 
-            num_l0=NUM_L0_NEURONS
+            num_l0=NUM_L0_NEURONS,
+            constant_in=C,
             )
-    logging.info(f"Results of random perceptron with default weigths {results}")
+    logging.info(f"Results of random perceptron with default weigths :{results}")
+    logging.info(f"Results of random perceptron weigths multiplied by constant C = {C}:{results_c}")
+    logging.info(f"Are results the same for multiplied weights: {(results_c[0] == results[0])}")
 
 
-def build_perceptron_network(len_inputs, num_l0):
+def build_perceptron_network(len_inputs, num_l0, constant_in):
     logging.info(f"Generating {len_inputs} inputs")
 
     inputs = generate_random_lst(len_inputs)
@@ -26,7 +28,7 @@ def build_perceptron_network(len_inputs, num_l0):
 
     logging.info(f"Generating {num_l0} perceptron-neurons on level 0")
 
-    weights_l0, biases_l0 = generate_wb_layer(num_inputs=len_inputs, num_neurons=num_l0)  
+    weights_l0, biases_l0 = generate_wb_layer(num_inputs=len_inputs, num_neurons=num_l0)
     logging.info(f"Level 0 neurons weights values: {weights_l0}\nBiases:\n{biases_l0}")
 
     l0_outputs = get_network_level_output(inputs=inputs, weights_level=weights_l0, biases_level=biases_l0)
@@ -38,7 +40,36 @@ def build_perceptron_network(len_inputs, num_l0):
 
     l1_outputs = get_network_level_output(inputs=l0_outputs, weights_level=weights_l1, biases_level=biases_l1)
 
-    return l1_outputs
+    
+    # Constant multiptlication
+    c_w_l0, c_b_l0 = get_wb_multiplied(weights_l0, biases_l0, constant_in)
+    logging.info(f"Level 0 neurons weights values multiplied by constant C = {constant_in}: {c_w_l0}\nBiases:\n{c_b_l0}")
+    c_l0_outputs = get_network_level_output(inputs=inputs, weights_level=c_w_l0, biases_level=c_b_l0)
+    logging.info(f"Level 0 output with altered weigths = {c_l0_outputs}")
+
+    c_w_l1, c_b_l1 = get_wb_multiplied(weights_l1, biases_l1, constant_in)
+    logging.info(f"Final level weights values multiplied by constant C = {constant_in}: {c_w_l1}\nBiases:\n{c_b_l1}")
+
+    c_l1_outputs = get_network_level_output(inputs=c_l0_outputs, weights_level=c_w_l1, biases_level=c_b_l1)
+
+    return l1_outputs, c_l1_outputs
+
+
+def get_wb_multiplied(weights, biases, mult_const):
+    assert len(weights.keys()) == len(biases.keys())
+    num_neurons = len(weights.keys())  
+    
+    updated_weights = {}
+    updated_biases = {}
+    for i in range(num_neurons):
+        updated_weights[i] = multiply_lst_by_const(weights[i], mult_const)
+        updated_biases[i] = biases[i] * mult_const
+    return updated_weights, updated_biases
+
+
+def multiply_lst_by_const(lst_in, constant_in):
+    new_lst = [i * constant_in for i in lst_in]
+    return new_lst
 
 
 def get_perceptron_output(inputs, weights, bias):
