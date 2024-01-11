ee7e153df3857b679c0ce75740c62c7b0df6aa96
Nikolai <nikdenof@gmail.com>
2024-01-11 17:08:19 +0100

    Codes are altered to work in python3

--- nn/src/mnist_svm.py
+++ nn/src/mnist_svm.py
@@ -14,14 +14,17 @@ from sklearn import svm
 
 def svm_baseline():
     training_data, validation_data, test_data = mnist_loader.load_data()
+    print("Data loaded")
     # train
     clf = svm.SVC()
+    print("SVM initialized")
     clf.fit(training_data[0], training_data[1])
+    print("Fit complete")
     # test
     predictions = [int(a) for a in clf.predict(test_data[0])]
     num_correct = sum(int(a == y) for a, y in zip(predictions, test_data[1]))
-    print "Baseline classifier using an SVM."
-    print "%s of %s values correct." % (num_correct, len(test_data[1]))
+    print("Baseline classifier using an SVM.")
+    print("%s of %s values correct." % (num_correct, len(test_data[1])))
 
 if __name__ == "__main__":
     svm_baseline()
